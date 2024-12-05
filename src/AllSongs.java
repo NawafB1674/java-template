582e9e3976444bafaf220173bf7e350b5037d1eb
NawafB1674 <nb7535@pleasantonusd.net>
2024-12-05 06:58:19 +0000

    commit message#

---
+++ src/AllSongs.java
@@ -0,0 +1,213 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AllSongs {
+   private static final Object[] get = null;
+   private List<Song> allSongs = new ArrayList<Song>();
+      AllSongs() {
+          allSongs.add(new Song("Faded", "Alan Walker", "English"));
+          allSongs.add(new Song("Unity", "Alan Walker", "English"));
+          allSongs.add(new Song("Specter", "Alan Walker", "English"));
+          allSongs.add(new Song("Lily", "Alan Walker", "English"));
+          allSongs.add(new Song("Take My Breath", "The Weekend", "English"));
+          allSongs.add(new Song("Starboy", "The Weekend", "English"));
+          allSongs.add(new Song("Popular", "The Weekend", "English"));
+          allSongs.add(new Song("HUMBLE.", "Kendrick Lamar", "English"));
+          allSongs.add(new Song("DNA.", "Kendrick Lamar", "English"));
+          allSongs.add(new Song("Alright.", "Kendrick Lamar", "English"));
+          allSongs.add(new Song("Not Like Us.", "Kendrick Lamar", "English"));
+          allSongs.add(new Song("What Makes You Beautiful", "One Direction", "English"));
+          allSongs.add(new Song("One Thing", "One Direction", "English"));
+          allSongs.add(new Song("Rock Me", "One Direction", "English"));
+    
+          allSongs.add(new Song("Moves Like Jagger", "Maroon 5", "English"));
+          allSongs.add(new Song("Memories", "Maroon 5", "English"));
+          allSongs.add(new Song("Wait", "Maroon 5", "English"));
+          allSongs.add(new Song("Cold", "Maroon 5", "English"));
+          allSongs.add(new Song("What Lovers Do", "Maroon 5", "English"));
+          allSongs.add(new Song("Girls Like You", "Maroon 5", "English"));
+          allSongs.add(new Song("California", "U2", "English"));
+          allSongs.add(new Song("Cedarwood Road", "U2", "English"));
+          allSongs.add(new Song("Every Breaking Wave", "U2", "English"));
+          allSongs.add(new Song("Hold Me Close", "U2", "English"));
+          allSongs.add(new Song("The Miracle(of Joey Ramon)", "U2", "English"));
+          allSongs.add(new Song("Raised by Wolves", "U2", "English"));
+          allSongs.add(new Song("Song for Someone", "U2", "English"));
+          allSongs.add(new Song("This is Where You Can Reach Me Now", "U2", "English"));
+          allSongs.add(new Song("The Troubles", "U2", "English"));
+          allSongs.add(new Song("Volcano", "U2", "English"));
+          allSongs.add(new Song("Thinking Out Loud", "Ed Sheeran", "English"));
+          allSongs.add(new Song("Shivers", "Ed Sheeran", "English"));
+          allSongs.add(new Song("Bad Habits", "Ed Sheeran", "English"));
+          allSongs.add(new Song("Beautiful People", "Ed Sheeran", "English"));
+          allSongs.add(new Song("Shape of You", "Ed Sheeran", "English"));
+          allSongs.add(new Song("I Built a Friend", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Paper Crown", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Must Have Been the Wind", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Devil Doesn't Bargain", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Jesus in LA", "Alec Benjamin", "English"));
+          allSongs.add(new Song("I Sent my Therapist to Therapy", "Alec Benjamin", "English"));
+          allSongs.add(new Song("If I Killed Someone for You", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Annabelle's Homework", "Alec Benjamin", "English"));
+          allSongs.add(new Song("Nothing Ever Changes", "Nico Collin", "English"));
+          allSongs.add(new Song("Timebomb", "Nico Collins", "English"));
+          allSongs.add(new Song("Alone", "Nico Collins", "English"));
+          allSongs.add(new Song("Ghost", "Nico Collins", "English"));
+          allSongs.add(new Song("Comatose", "Skillet", "English"));
+          allSongs.add(new Song("Monster", "Skillet", "English"));
+          allSongs.add(new Song("Hero", "Skillet", "English"));
+          allSongs.add(new Song("Awake and Alive", "Skillet", "English"));
+          allSongs.add(new Song("The Resistance", "Skillet", "English"));
+          allSongs.add(new Song("Whispers in the Dark", "Skillet", "English"));
+          allSongs.add(new Song("Telescope", "Starset", "English"));
+          allSongs.add(new Song("Dark on Me", "Starset", "English"));
+          allSongs.add(new Song("It Has Begun", "Starset", "English"));
+          allSongs.add(new Song("Carnivore", "Starset", "English"));
+          allSongs.add(new Song("Let it Die", "Starset", "English"));
+          allSongs.add(new Song("Perfect Machine", "Starset", "English"));
+          allSongs.add(new Song("On My Own", "Ashes Remain", "English"));
+          allSongs.add(new Song("Without You", "Ashes Remain", "English"));
+          allSongs.add(new Song("End of Me", "Ashes Remain", "English"));
+          allSongs.add(new Song("Right Here", "Ashes Remain", "English"));
+          allSongs.add(new Song("Change my Life", "Ashes Remain", "English"));
+          allSongs.add(new Song("Fly Away", "The Fat Rat", "English"));
+          allSongs.add(new Song("Mayday", "The Fat Rat", "English"));
+          allSongs.add(new Song("Hiding in the Blue", "The Fat Rat", "English"));
+          allSongs.add(new Song("手掌心(Palm)", "丁噹(Della Ding)", "Chinese"));
+          allSongs.add(new Song("最好不要再遇见你(Better to Never Meet Again)", "丁噹(Della Ding)", "Chinese"));
+          allSongs.add(new Song("海底(Ocean Floor)", "凤凰传奇(Phoenix Legend)", "Chinese"));
+          allSongs.add(new Song("最炫民族风(The Most Dazzling Folk Style)", "凤凰传奇(Phoenix Legend)", "Chinese"));
+          allSongs.add(new Song("自由飞翔(Fly Freely)", "凤凰传奇(Phoenix Legend)", "Chinese"));
+          allSongs.add(new Song("我从草原来(I Come From the Plains)", "凤凰传奇(Phoenix Legend)", "Chinese"));
+          allSongs.add(new Song("成仙(Become God)", "王朝(Wang Chao)", "Chinese"));
+          allSongs.add(new Song("坐忘道(Way of Forgetting)", "王朝(Wang Dynasty)", "Chinese"));
+          allSongs.add(new Song("Dramaturgy", "Eve", "Japanese"));
+          allSongs.add(new Song("Fight Song", "Eve", "Japanese"));
+          allSongs.add(new Song("How to Eat Life", "Eve", "Japanese"));
+          allSongs.add(new Song("木偶戏(Puppet Show)", "玥夏(YueXia)", "Chinese"));
+          allSongs.add(new Song("黑暗里(Within Darkness)", "玥夏(YueXia)", "Chinese"));
+          allSongs.add(new Song("百鬼夜行(Night Parade of a Hundred Demons)", "玥夏(YueXia)", "Chinese"));
+          allSongs.add(new Song("Believer", "Imagine Dragons", "English"));
+          allSongs.add(new Song("Radioactive", "Imagine Dragons", "English"));
+    
+          allSongs.add(new Song("Break Free", "Ariana Grande", "English"));
+          allSongs.add(new Song("Side to Side", "Ariana Grande", "English"));
+          allSongs.add(new Song("7 Rings", "Ariana Grande", "English"));
+          allSongs.add(new Song("Yes, And?", "Ariana Grande", "English"));
+          allSongs.add(new Song("We Can't be Friends", "Ariana Grande", "English"));
+          allSongs.add(new Song("Thank U Next", "Ariana Grande", "English"));
+          allSongs.add(new Song("Dynamite", "Bangtan Boys", "English"));
+          allSongs.add(new Song("ON", "Bangtan Boys", "English"));
+          allSongs.add(new Song("Butter", "Bangtan Boys", "English"));
+          allSongs.add(new Song("My Universe", "Bangtan Boys", "English"));
+          allSongs.add(new Song("Dreamers", "Jungkook", "English"));
+          allSongs.add(new Song("Seven", "Jungkook", "English"));
+          allSongs.add(new Song("Left and Right", "Jungkook", "English"));
+      }
+     
+      //Find all the songs from the author.
+      public List<Song> findAllSongsFromAuthor(String authorName) {
+          List<Song> result = new ArrayList<>();
+          for(Song song:allSongs) {
+              if(song.getAuthorName().equals(authorName)){
+                  result.add(song);
+              }
+          }
+          return result;
+      }
+   
+   
+      //Find the author through a song.
+      public String findAuthorFromSongName(String songName) {
+          for(Song song:allSongs) {
+              if(song.getSongName().equals(songName)){
+                  return song.getAuthorName();
+              }
+          }
+          return "Sorry, this song is currently not in the database.";
+      }
+   
+   
+      //Through the user's song input, find all the songs who were sung by the same author.
+      public List<Song> findAllSongsFromSameAuthorOfSong(String songName) {
+          return findAllSongsFromAuthor(findAuthorFromSongName(songName));
+      }
+     
+      //------------
+     
+      //Get songs from the same language as another song.
+   
+   
+      public List<Song> listAllSongsfromLanguage(String songLanguage) {
+          List<Song> result = new ArrayList<>();
+          for(Song song:allSongs) {
+              if(song.getLanguage().equals(songLanguage)){
+                  result.add(song);
+              }
+          }
+          return result;
+      }
+   
+   
+      public String findLanguageFromSongName(String songName) {
+          for(Song song:allSongs) {
+              if(song.getSongName().equals(songName)){
+                  return song.getLanguage();
+              }
+          }
+          return "Sorry, this song is currently not in the database.";
+      }
+   
+   
+      public List<Song> findAllSongsFromSameLanguage(String songName) {
+          return listAllSongsfromLanguage(findLanguageFromSongName(songName));
+      }
+   
+   
+      //Get songs from the same language as the author.
+      public List<Song> listAllAuthorsFromLanguage(String authorLanguage) {
+          List<Song> result = new ArrayList<>();
+          for(Song song:allSongs) {
+              if(song.getLanguage().equals(authorLanguage)){
+                  result.add(song);
+              }
+          }
+          return result;
+      }
+   
+   
+      public String findLanguageFromAuthorName(String authorName) {
+          for(Song song:allSongs) {
+              if(song.getAuthorName().equals(authorName)){
+                  return song.getLanguage();
+              }
+          }
+          return "Sorry, this song is currently not in the database.";
+      }
+     
+      public List<Song> findAllAuthorsFromSameLanguage(String authorName) {
+          return listAllAuthorsFromLanguage(findLanguageFromAuthorName(authorName));
+   
+   
+   ArrayList<Song> English = new ArrayList<Song>();
+        for(int i = 0; i<AllSongs.size(); i++){
+            if (AllSongs.get(i).getLanguage = "English"){
+           	English.add(i);
+        }
+}
+
+
+   }
+
+
+   // public List<Song> songsFromSameLanguage(String songLanguage){
+   //     List<Song> result = new ArrayList<>();
+   //     for(Song song:allSongs){
+   //         if(song.getLanguage().equals(songLanguage)){
+   //             result.add(song);
+   //         }
+   //     }
+   // }
+}
--- src/App.java
+++ src/App.java
@@ -1,6 +1,188 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+import java.util.List;
+import java.util.Scanner;
+public class App
+{
+   //CREATE INSTANCE VARIABLES HERE. Ensure they are static.
+   //May want to create an array of goodbye answers.
+ static String[] goodBye = {"bye"};
+ public static void main(String[] args){
+   try (Scanner in = new Scanner (System.in)) {
+     Scanner in2 = new Scanner (System.in); //Creates a second scanner object. Not needed(?).
+    
+     String[] disagree = {"No", "Nah", "Nope"};
+     String[] agree = {"Yes", "Yeah", "Okay", "Of course"};
+     AllSongs allSongs = new AllSongs();
+     while (true){
+       System.out.println("Hello, user! Are you here to find a song to listen to?");
+       String userResp = in.nextLine(); //in.nextLine() uses the scanner object to get the user's response as a String
 
+
+       if (findArray(userResp, disagree)){
+         System.out.println("Apologies, then I cannot assist you.");
+         break;
+       }
+
+
+
+
+       else if (findArray(userResp, agree)){
+         System.out.println("Are you looking for a completely new song,"
+         + " or do you already have an author you want recommendations about in mind?" +
+         " By the way, you can exit at anytime by typing 'Bye'.");
+         userResp = in2.nextLine();
+
+
+         if(findString(userResp, "Bye")){
+           System.out.println("Goodbye!");
+           return;
+         }
+
+
+         if(findString(userResp, "Author")){
+           System.out.println("Alright, which author would you like to hear more songs of?");
+           userResp = in.nextLine();
+
+
+           if(findString(userResp, "Bye")){
+             System.out.println("Goodbye!");
+             return;
+           }
+
+
+           else{
+             System.out.println("This direction of inquiry is currently unfinished."
+             + "Please return to the start and pick the other option.");
+           }
+         }
+
+
+
+
+         else if(findString(userResp, "New Song")){
+           System.out.println("Do you prefer English, Chinese, or Japanese songs?");
+           userResp = in.nextLine();
+
+
+           if(findString(userResp, "Bye")){
+             System.out.println("Goodbye!");
+             return;
+           }
+
+
+
+
+           else if(findString(userResp, "English")){
+             System.out.println("Here are some authors who sing in English:");
+             System.out.println("Alec Benjamin.");
+             System.out.println("The Weekend.");
+             System.out.println("Alan Walker.");
+             System.out.println("Kendrick Lamar.");
+             System.out.println("This system is currently not very user-friendly. Typing would loop you back to the start." +
+             "When going through the dialogue again, please choose 'author' if you have someone in mind this time." +
+             "If not, choose a different language from the 'new songs' list.");    
+
+
+
+
+             //for(int i = 0; English.size()>i; i++){
+               //System.out.println(English.get(i).getAuthorName());
+            // }
+             userResp = in2.nextLine();
+
+
+             // List<Song> authors = allSongs.songsFromSameLanguage("English");
+             // for(int i = 0; i < authors.size())); i++){
+             //   Song s = authors.get(i);
+             //   System.out.println(s.getAuthorName());
+             // }
+           }
+
+
+           else if(findString(userResp, "Chinese")){
+             System.out.println("Here are some authors who sing in Chinese:");
+             System.out.println("丁噹(Della Ding).");
+             System.out.println("凤凰传奇(Phoenix Legend).");
+             System.out.println("王朝(Wang Chao).");
+             System.out.println("玥夏(YueXia).");
+             System.out.println("This system is currently not very user-friendly. Typing would loop you back to the start." +
+             "When going through the dialogue again, please choose 'author' if you have someone in mind this time." +
+             "If not, choose a different language from the 'new songs' list.");
+             userResp = in2.nextLine();
+           }
+
+
+           else if(findString(userResp, "Japanese")){
+             System.out.println("Here are some authors who sing in Japanese:");
+             System.out.println("Eve.");
+             System.out.println("This system is currently not very user-friendly. Typing would loop you back to the start." +
+             "When going through the dialogue again, please choose 'author' if you have someone in mind this time." +
+             "If not, choose a different language from the 'new songs' list.");             
+             userResp = in2.nextLine();
+           }
+
+
+           else{
+             System.out.println("Sorry, please choose one of the three languages.");
+           }
+         }
+
+
+
+
+         else{
+           System.out.println("Sorry, I don't quite understand. Could you repeat your statement?");
+           userResp = in.nextLine();
+
+
+           if(findString(userResp, "Bye")){
+             System.out.println("Goodbye!");
+             return;
+           }
+         }
+       }
+
+
+       if (findString(userResp, "Coders")){
+         System.out.println("A time not so long ago, in a galaxy not very far away...");
+         System.out.println("Two coders were scratching their head over a random piece of code.");
+         System.out.println("This Chatbot was coded by:");
+         System.out.println("Nawaf Bhuiyan.");
+         System.out.println("Yu Zhu.");
+         System.out.println("Thanks for using our chatbot!");
+         break;
+       }
+
+
+       else{
+         System.out.println("Apologies, I don't quite understand.");
+       }
+     }
+    
+    
+     //List<Song> findArraySongsFromSameAuthor = new AllSongs().findArrayAllSongsFromSameAuthorOfSong(SecondResp);
+     in.close();
+     in2.close();
+   }
+ } 
+      
+ private static boolean findArray(String userResponse, String[] array) {
+   for(int i = 0; i < array.length; i++) {
+     if( userResponse.toLowerCase().contains(array[i].toLowerCase())){
+       return true;
+     }
+   }
+   return false;
+ }
+
+
+ private static boolean findString(String userResponse, String str){
+   for(int i = 0; i < userResponse.length(); i++) {
+     if( userResponse.toLowerCase().contains(str.toLowerCase())){
+       return true;
+     }
+   }
+   return false;
+ }
+
+
+ }
