cebac0073d1bbdfb3416067591812881d3a397b6
NawafB1674 <nb7535@pleasantonusd.net>
2024-12-10 09:26:21 -0800

    Update Song.java

--- src/Song.java
+++ src/Song.java
@@ -1,36 +1,36 @@
 public class Song {
-   private String songName;
-   private String authorName;
-   private String language;
-//------------
-   public Song(String s, String a, String l){
-       songName = s;
-       authorName = a;
-       language = l;
-   }
-//------------  
-   //Getters
-   public String getSongName(){
-       return songName;
-   }
-   public String getAuthorName(){
-       return authorName;
-   }
-   public String getLanguage(){
-       return language;
-   }
-//------------
-   //Setters
-   public void setSongName(String s){
-       songName = s;
-   }
-   public void setAuthorName(String a){
-       authorName = a;
-   }
-   public void setLanguage(String l){
-       language = l;
-   }
-   public String toString(){
-       return songName + " by " + authorName + "(" + language + ")";
-   }
-}
+    private String songName;
+    private String authorName;
+    private String language;
+ //------------
+    public Song(String s, String a, String l){
+        songName = s;
+        authorName = a;
+        language = l;
+    }
+ //------------  
+    //Getters
+    public String getSongName(){
+        return songName;
+    }
+    public String getAuthorName(){
+        return authorName;
+    }
+    public String getLanguage(){
+        return language;
+    }
+ //------------
+    //Setters
+    public void setSongName(String s){
+        songName = s;
+    }
+    public void setAuthorName(String a){
+        authorName = a;
+    }
+    public void setLanguage(String l){
+        language = l;
+    }
+    public String toString(){
+        return songName + " by " + authorName + "(" + language + ")";
+    }
+ }
